9b65a2b941f707f3c239ed72e862bf78b2ff74fd
adikrisnaffa <adikrisnanugraha@gmail.com>
2024-01-09 11:45:29 +0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,12 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
   },
+  build: {
+    esbuild: {
+      keepAlive: true,
+    },
+  },
   preview: {
-    port:3001
-  }
+    port: 3001,
+  },
 });
